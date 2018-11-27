e4362bdbea671546ada4ba8334432ccd9bb4eb24
zarya <suhail.parmar@gmail.com>
2018-11-27 15:48:05 +0000

    Use the enriched queue as the source

--- lib/config.py
+++ lib/config.py
@@ -11,3 +11,14 @@ api_client_secret = getenv(
     'API_SECRET', "supersecret")  # Not real value
 
 log_file = getenv('LOG_FILE', 'motorway_enricher.log')
+
+rabbit_host = getenv('MQ_HOST', 'localhost')
+rabbit_port = getenv('MQ_PORT', 5672)
+rabbit_source_queue = getenv('MQ_SOURCE_QUEUE', 'M6_Enriched')
+rabbit_dlqueue = getenv('MQ_DL_QUEUE', 'M6_Dead_Letter')
+rabbit_username = getenv('MQ_USERNAME', 'guest')
+rabbit_password = getenv('MQ_PASSWORD', 'guest')
+rabbit_exchange = getenv('MQ_EXCHANGE', 'MotorwayExchange')
+rabbit_dl_routing_key = getenv('MQ_DL_ROUTING_KEY', 'M6_DL')
+rabbit_vhost = getenv('MQ_VHOST', 'motorway_vhost')
+
