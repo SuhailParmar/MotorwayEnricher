228386ad09c0c07f50732bfb00b22054bbcd3999
zarya <suhail.parmar@gmail.com>
2019-01-07 18:41:43 +0000

    Created polling function to query an account between specified timerange

--- lib/twitter_client.py
+++ lib/twitter_client.py
@@ -1,8 +1,12 @@
 import twitter
 import logging
 import lib.config as config
+from lib.utils import Utils
+from datetime import datetime
+from dateutil.parser import parse
 
 th_logger = logging.getLogger("TwitterClient")
+ut = Utils()
 
 
 class TwitterClient:
@@ -20,3 +24,43 @@ class TwitterClient:
                            consumer_secret=config.twitter_api_secret,
                            access_token_key=config.twitter_at_token,
                            access_token_secret=config.twitter_at_secret)
+
+    def poll_user_at_time(self, handle, timestamp):
+        """
+        Handle: Twitter Account Name @SuhailParmar
+        Timestamp: Time want to see if they tweeted at that time
+        """
+        n = 5  # Number of tweets to grab at one time
+        i = 0  # Index of the tweets array
+        max_tweets = 100  # Maximum number of tweets to scrape
+        from_timestamp, until_timestamp = ut.calc_daterange_boundaries(
+            timestamp, 2)
+        timeline_in_date_range = []  # Finished
+
+        while i < max_tweets:
+            timeline = self.api.GetUserTimeline(screen_name=handle, count=i+n)
+            unvalidated_tweets = timeline[i:]
+
+            for ind, tweet in enumerate(unvalidated_tweets):
+                tweet_timestamp = parse(tweet.created_at)
+                th_logger.debug(
+                    'Validating if {0} is in the bounds of {1} - {2}'.format(
+                        tweet_timestamp, from_timestamp, until_timestamp))
+
+                if ut.within_daterange(
+                        tweet_timestamp, from_timestamp, until_timestamp):
+
+                    timeline_in_date_range.append(tweet)
+                    continue
+
+                elif len(timeline_in_date_range) > 0:
+                    return timeline_in_date_range
+
+                th_logger.debug(
+                    'Havent found a tweet in the date range in {} tweets'.format(i))
+            i += n
+
+        th_logger.info(
+            'Didnt find a tweet in the date range in {} tweets'.format(i))
+
+        return False
