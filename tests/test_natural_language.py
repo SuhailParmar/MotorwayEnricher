5edb429720419e26da148dc8037107d00334860a
zarya <suhail.parmar@gmail.com>
2019-01-09 15:11:06 +0000

    Added tokenzer and lowercase functions

---
+++ tests/test_natural_language.py
@@ -0,0 +1,15 @@
+from lib.natural_language import NaturalLanguage
+
+
+class TestNaturalLanguage:
+
+    nl = NaturalLanguage()
+
+    def test_lower_case_convertion(self):
+        response = self.nl.tokenize("Hello, my name is Suhail")
+        assert response == ["Hello", ",", "my", "name", "is", "Suhail"]
+
+    def test_convert_to_lower_case(self):
+        response = self.nl.convert_arr_to_lowercase(
+            ["J25", ",", "my", "name", "is", "Suhail"])
+        assert response == ["j25", ",", "my", "name", "is", "suhail"]
