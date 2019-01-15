d4672c62d56cb7fb4c5e029a3196195dd4d2de0f
zarya <suhail.parmar@gmail.com>
2019-01-15 19:00:59 +0000

    One cluster function to output values from clusters

--- lib/document_clusterer.py
+++ lib/document_clusterer.py
@@ -11,7 +11,7 @@ class DocumentClusterer:
 
         self.nc = NaturalLanguage()
 
-        self.tdidf_vectorizer = TfidfVectorizer(
+        self.tfidf_vectorizer = TfidfVectorizer(
             min_df=0.1, max_df=0.9, stop_words='english',
             use_idf=True, ngram_range=(1, 3)
         )
@@ -22,24 +22,47 @@ class DocumentClusterer:
         """
         Create the matrix based on the documents provided
         """
-
-        self.tdidf_matrix = self.tdidf_vectorizer.fit_transform(documents)
-
+        self.tdidf_matrix = self.tfidf_vectorizer.fit_transform(documents)
         dist = 1 - cosine_similarity(self.tdidf_matrix)  # Required later
         return self.tdidf_matrix
 
-    def kmeans_cluster(self):
+    def cluster(self):
         """
         Based on the original documents provided assign each
         a clustering number.
         """
-        clusters = 3
-        km = KMeans(n_clusters=clusters)
+        # Choose the number of clusters to create
+        # Potentially use the GMM model
+        n_clusters = 3
+        km = KMeans(n_clusters=n_clusters)
         km.fit(self.tdidf_matrix)
+
         clusters = km.labels_.tolist()
-        return clusters
+        cluster_centeroids = km.cluster_centers_.argsort()[:, ::-1]
+
+        # All of the vocabulary in the tfidf matrix
+        tfidf_matrix_terms = self.tfidf_vectorizer.get_feature_names()
+
+        # Get the indexs of the values closest to the centroid
+        # The indexes correlate to the position in the tfidf matrix
+        values_closest_to_centroid = 5  # Return the n closest values to that centroid
+        relvant_values_per_cluster = {}
+        terms = []
+        for i in range(0, n_clusters):
+            # Get the indexs for each cluster
+            tfidf_matrix_indexs = cluster_centeroids[i, :values_closest_to_centroid]
+            print("Cluster {}:".format(i))
+            for index in tfidf_matrix_indexs:
+                term = tfidf_matrix_terms[index]
+                print(term)
+                terms.append(term)
+            print()
+
+            relvant_values_per_cluster[i] = terms
+
+        return relvant_values_per_cluster
 
     def to_dataframe(self, docs, cluster):
         df = pd.DataFrame(docs, index=cluster, columns=['original_tweet'])
         df.index.rename('cluster', inplace=True)
-        return df
+        return df.sort_index()
--- tests/test_document_clusterer.py
+++ tests/test_document_clusterer.py
@@ -7,10 +7,10 @@ class TestDocumentClusterer:
     original_tweet = "m6 congestion traffic j25 southbound south"
 
     documents = [
-        original_tweet,
+        # original_tweet,
         "Lorry Caused The Accident On the m6",
         "M6 incident was caused by overturned lorry",
-        "Lorry battered on M6",
+        "Lorry is flipped over on M6",
         "M6 accident on J25 south",
         "Seems to be an accident on the m6 J25",
     ]
@@ -19,15 +19,18 @@ class TestDocumentClusterer:
         # Key words extracted from original tweet
         # Matrix expects strings, not already parsed arrays
         matrix = self.dc.create_tf_idf_matrix(self.documents)
-        clusters = self.dc.kmeans_cluster()
-        assert len(clusters) > 0
-        assert min(clusters) == 0
-        assert max(clusters) == 2
+        v_p_clusters = self.dc.cluster()
+        assert len(v_p_clusters) > 0
+        assert min(v_p_clusters) == 0
+        assert max(v_p_clusters) == 2
+        print(v_p_clusters)
+        assert False
 
+    """
     def test_create_pandas_dataframe(self):
         matrix = self.dc.create_tf_idf_matrix(self.documents)
-        clusters = self.dc.kmeans_cluster()
+        clusters = self.dc.cluster()
         df = self.dc.to_dataframe(self.documents, clusters)
-        df = df.sort_index()
         print(df.head(10))
         assert False
+    """
