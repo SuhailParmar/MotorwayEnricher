61f3746d970cbe3a3ed9263cabd51f7aec6faefb
zarya <suhail.parmar@gmail.com>
2019-01-17 10:27:59 +0000

    Simplified clusterer - included logging

--- lib/document_clusterer.py
+++ lib/document_clusterer.py
@@ -3,6 +3,9 @@ from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
 from lib.natural_language import NaturalLanguage
 import pandas as pd
+import logging
+
+dc_logger = logging.getLogger("DocumentClusterer")
 
 
 class DocumentClusterer:
@@ -26,14 +29,11 @@ class DocumentClusterer:
         dist = 1 - cosine_similarity(self.tdidf_matrix)  # Required later
         return self.tdidf_matrix
 
-    def cluster(self):
-        """
-        Based on the original documents provided assign each
-        a clustering number.
-        """
+    def cluster(self, n_clusters=3):
+        # Based on the original documents provided assign each
+        # a clustering number.
         # Choose the number of clusters to create
         # Potentially use the GMM model
-        n_clusters = 3
         km = KMeans(n_clusters=n_clusters)
         km.fit(self.tdidf_matrix)
 
@@ -50,13 +50,17 @@ class DocumentClusterer:
         terms = []
         for i in range(0, n_clusters):
             # Get the indexs for each cluster
-            tfidf_matrix_indexs = cluster_centeroids[i, :values_closest_to_centroid]
+            tfidf_matrix_indexs = cluster_centeroids[i,
+                                                     :values_closest_to_centroid]
+            dc_logger.info("Cluster {}:".format(i))
             print("Cluster {}:".format(i))
             for index in tfidf_matrix_indexs:
                 term = tfidf_matrix_terms[index]
+                dc_logger.info(term)
                 print(term)
                 terms.append(term)
-            print()
+            dc_logger.info('---')
+            print('---')
 
             relvant_values_per_cluster[i] = terms
 
@@ -66,3 +70,19 @@ class DocumentClusterer:
         df = pd.DataFrame(docs, index=cluster, columns=['original_tweet'])
         df.index.rename('cluster', inplace=True)
         return df.sort_index()
+
+    def strip_words(self, documents, kws):
+        dc_logger.debug(documents)
+        for i, document in enumerate(documents):
+            doc_temp = document
+            for word in kws:
+                if word in document:
+                    doc_temp = doc_temp.replace(word, "")
+            documents[i] = doc_temp
+        dc_logger.debug(documents)
+        return documents
+
+    def main(self, documents):
+        self.create_tf_idf_matrix(documents)
+        v_p_clusters = self.cluster()
+        dc_logger.info(v_p_clusters)
--- tests/test_document_clusterer.py
+++ tests/test_document_clusterer.py
@@ -3,34 +3,25 @@ from lib.document_clusterer import DocumentClusterer
 
 class TestDocumentClusterer:
 
-    dc = DocumentClusterer()
     original_tweet = "m6 congestion traffic j25 southbound south"
 
     documents = [
-        # original_tweet,
-        "Lorry Caused The Accident On the m6",
-        "M6 incident was caused by overturned lorry",
-        "Lorry is flipped over on M6",
-        "M6 accident on J25 south",
-        "Seems to be an accident on the m6 J25",
+        "Lorry Caused The Accident On the m6 j25",
+        "m6 incident was caused by overturned lorry",
+        "Lorry is flipped over on m6",
+        "m6 accident on j25 south",
+        "Seems to be an accident on the m6 j25",
     ]
 
-    def test_kmeans_clusterer(self):
-        # Key words extracted from original tweet
-        # Matrix expects strings, not already parsed arrays
-        matrix = self.dc.create_tf_idf_matrix(self.documents)
-        v_p_clusters = self.dc.cluster()
-        assert len(v_p_clusters) > 0
-        assert min(v_p_clusters) == 0
-        assert max(v_p_clusters) == 2
-        print(v_p_clusters)
-        assert False
+    def test_word_stripper(self):
+        dc = DocumentClusterer()
+        docs = dc.strip_words(self.documents, self.original_tweet.split())
+        assert len(docs) == 5
+        assert docs[0] == "Lorry Caused The Accident On the  "
 
-    """
-    def test_create_pandas_dataframe(self):
-        matrix = self.dc.create_tf_idf_matrix(self.documents)
-        clusters = self.dc.cluster()
-        df = self.dc.to_dataframe(self.documents, clusters)
-        print(df.head(10))
-        assert False
-    """
+    def test_cluster(self):
+        dc = DocumentClusterer()
+        docs = dc.strip_words(self.documents, self.original_tweet)
+        dc.create_tf_idf_matrix(docs)
+        x = dc.cluster()
+        assert len(x) == 3
