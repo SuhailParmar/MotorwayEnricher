421a139015f2f62e89a89eab5d65ea1527cbad7e
zarya <suhail.parmar@gmail.com>
2019-01-29 11:33:10 +0000

    Organised relevance.

--- lib/relevance_checker.py
+++ lib/relevance_checker.py
@@ -1,3 +1,5 @@
+from lib.utils import Utils
+from lib.natural_language import NaturalLanguage
 import logging
 
 rl_logger = logging.getLogger("RelevanceChecker")
@@ -6,101 +8,105 @@ rl_logger = logging.getLogger("RelevanceChecker")
 class RelevanceChecker:
 
     def __init__(self):
-        self.number_to_words = {
-            1: "one",
-            2: "two",
-            3: "three",
-            4: "four",
-            5: "five",
-            6: "six",
-            7: "seven",
-            8: "eight",
-            9: "nine",
-            14: "fourteen",
-            15: "fifteen",
-            25: "twenty-five"
+
+        self.nl = NaturalLanguage()
+
+        self.directions = {
+            "a": ["anti-clockwise"],
+            "c": ["clockwise", "c/w"],
+            "e": ["eastbound", "e/bound", "east"],
+            "n": ["northbound", "n/bound", "north"],
+            "s": ["southbound", "s/bound", "south"],
+            "w": ["westbound", "w/bound", "west"],
         }
 
     def create_eng_keywords_from_tweet(self, original_tweet):
         """
         Generate english keywords from the original tweet
-        Important information: motorway, junctions, direction
+        Important information: motorway, junctions
+        Other less-important info: reasons, cities
+        return ([motorway, junctions**], [other_info])
         """
-        motorway = []
+        motorway_and_junction = []
         mw_number = str(original_tweet["motorway"])
-        motorway.append("M" + mw_number)
-
-        junctions = []
+        motorway_and_junction.append("M" + mw_number)
         for j in original_tweet["junction"]:
-            junctions.append("J" + str(j))
-            """
-            junctions.append("junction " + str(j))
-            try:
-                junctions.append("junction " + self.number_to_words[j])
-            except KeyError as e:
-                rl_logger.warn(
-                    'Junction {} cannot be converted into a word'.format(e))
-            """
-        directions = []
+            motorway_and_junction.append("J" + str(j))
 
+        # Directions
+        other_info = []
         if original_tweet["direction"] == "n":
-            directions.append("northbound")
-            directions.append("north")
-            directions.append("n/bound")
+            other_info.append("northbound")
+            other_info.append("north")
+            other_info.append("n/bound")
         elif original_tweet["direction"] == "e":
-            directions.append("eastbound")
-            directions.append("east")
-            directions.append("e/bound")
+            other_info.append("eastbound")
+            other_info.append("east")
+            other_info.append("e/bound")
         elif original_tweet["direction"] == "s":
-            directions.append("southbound")
-            directions.append("south")
-            directions.append("s/bound")
+            other_info.append("southbound")
+            other_info.append("south")
+            other_info.append("s/bound")
         elif original_tweet["direction"] == "w":
-            directions.append("westbound")
-            directions.append("west")
-            directions.append("w/bound")
+            other_info.append("westbound")
+            other_info.append("west")
+            other_info.append("w/bound")
         elif original_tweet["direction"] == "a":
-            directions.append("anti-clockwise")
+            other_info.append("anti-clockwise")
         elif original_tweet["direction"] == "c":
-            directions.append("clockwise")
+            other_info.append("clockwise")
 
-        rl_logger.debug("Constructed Keywords:\n{0}{1}{2}".format(motorway, junctions, directions))
-        return (motorway, junctions, directions)
+        reasons_strings = original_tweet["reason"].split(" ")
+        if isinstance(reasons_strings, list):
+            for r in reasons_strings:
+                other_info.append(r)
+        else:
+            other_info.append(reasons_strings)
 
-    def find_relevant_tweets(self, tweets, motorway, junctions, directions):
-        """
-        Only choose a subset of tweets which are about:
-        The motorway in question
-        The junction in question
-        Direction is currently irrelevant
-        """
+        for c in original_tweet["closest_cities"]:
+            split = c.split(" ")
+            for v in split:
+                if v is not '/':
+                    other_info.append(v.lower())
 
-        relevant_tweets = []
+        rl_logger.debug("Constructed Keywords:\n{0}{1}".format(
+            motorway_and_junction, other_info))
+        return (motorway_and_junction, other_info)
 
-        for tweet in tweets:
-            rl_logger.debug('Relevance checking: {}'.format(tweet))
-            if motorway[0] not in tweet:
-                rl_logger.debug('Tweet is not about the M6.')
-                continue
+    def is_tweet_relevant(self, tweet, original_direction):
+        rl_logger.info("Checking relevance of\n{0}".format(tweet.text))
 
-            junction_flag = False
-            for junction in junctions:
-                if junction in tweet:
-                    junction_flag = True
+        if tweet.user.screen_name is "@epsomcanine":
+            rl_logger.info('Skipping Dog tweet: {}'.format(tweet.text))
+            return False
 
-            if not junction_flag:
-                rl_logger.debug(
-                    'Tweet is not about junction(s) {}.'.format(junctions))
-                continue
+        # The tweet's timestamp is in bounds of from_timestamp and until_timestamp
+        lowercase_tweet = self.nl.convert_to_lowercase(tweet.text)
+        lowercase_tweet = Utils.strip_link_from_tweet(lowercase_tweet)
 
-            # TODO Maybe have 2 tiers based on direction??
-            relevant_tweets.append(tweet)
+        # ignore retweets
+        if lowercase_tweet[0] == 'r' and lowercase_tweet[1] == 't':
+            rl_logger.info('Skipping Retweet: {}'.format(tweet.text))
+            return False
 
-        rl_logger.info('Found {0} RELEVANT tweets!'.format(
-            len(relevant_tweets)))
+        if not self.is_relevant_direction(original_direction, tweet):
+            rl_logger.info('Tweet is not about direction {}'.format(
+                original_direction))
+            return False
 
-        if len(relevant_tweets) > 0:
-            for i, relevant_tweet in enumerate(relevant_tweets):
-                rl_logger.info(str(i) + ') ' + relevant_tweet)
+        rl_logger.info('** Keeping tweet:\n {}'.format(tweet.text))
+        return lowercase_tweet
 
-        return relevant_tweets
+    def is_relevant_direction(self, original_direction, tweet):
+        if original_direction is "a":
+            # clockwise is in 'anti-clockwise' so search
+            # entire tweet for "anti-clockwise"
+            return self.directions["a"][0] in tweet
+
+        for k, v in self.directions.items():
+            if k is not original_direction:
+                for value in v:
+                    if value in tweet:
+                        return False
+
+        return True
